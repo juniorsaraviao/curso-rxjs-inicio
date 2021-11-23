840efa73092b3fd32dce79e8b5bfbb1a7c3aa1ac
Junior Saravia <junior.saravia.o@uni.pe>
2021-11-22 22:43:28 -0500

    First observable

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
+import { Observable } from 'rxjs'
 
 
-console.log('Hola Mundo!');
+// const obs$ = Observable.create();
+
+const obs$ = new Observable<string>( subs => {
+    subs.next('Hola')
+    subs.next('Mundo')
+
+    subs.next('Hola')
+    subs.next('Mundo')
+
+    subs.complete()
+
+    subs.next('Hola')
+    subs.next('Mundo')
+});
 
 
-
-
-
-
-
+obs$.subscribe( console.log )
