8e665917509365fb8a9da6da7236de5ddaf87c3a
Junior Saravia <junior.saravia.o@uni.pe>
2021-11-22 23:08:42 -0500

    Observer and suscriber

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,10 @@
-import { Observable } from 'rxjs'
+import { Observable, Observer } from 'rxjs'
+
+const observer: Observer<any> = {
+    next: value => console.log('next [next]: ', value),
+    error: error => console.warn('error [obs]: ', error),
+    complete: () => console.info('Complete [obs]')
+}
 
 
 // const obs$ = Observable.create();
@@ -9,12 +15,23 @@ const obs$ = new Observable<string>( subs => {
 
     subs.next('Hola')
     subs.next('Mundo')
-
+    
+    //subs.error('Something went wrong')
+    
     subs.complete()
 
     subs.next('Hola')
     subs.next('Mundo')
+
 });
 
 
-obs$.subscribe( console.log )
+// First way
+// obs$.subscribe({
+//     next: valor => console.log('next: ', valor),
+//     error: error => console.warn('error: ', error),
+//     complete: () => console.info('Completed')
+// })
+
+// Second way
+obs$.subscribe(observer);
