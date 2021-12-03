eedbbb50d1885b3a04d787e7ea174253dc4f3c7f
Junior Saravia <junior.saravia.o@uni.pe>
2021-12-02 23:20:21 -0500

    working with sampleTime

---
+++ src/time-operators/03-sampleTime.ts
@@ -0,0 +1,8 @@
+import { fromEvent, map, sampleTime } from "rxjs";
+
+const click$ = fromEvent<PointerEvent>( document, 'click' );
+
+click$.pipe(
+    sampleTime(2000),
+    map( ({x, y}) => ({x, y}) )
+).subscribe( console.log );
