696ec3a94db5cca78b5b2162a66f6563467c2f0f
Junior Saravia <junior.saravia.o@uni.pe>
2021-12-20 22:49:31 -0500

    wirking with concat

---
+++ src/combinators/03-concat.ts
@@ -0,0 +1,9 @@
+import { take } from 'rxjs/operators';
+import { concat, interval } from 'rxjs';
+const interval$ = interval(1000);
+
+concat(
+    interval$.pipe( take(3) ),
+    interval$.pipe( take(2) ),
+    [1,2,3,4]
+).subscribe( console.log );
