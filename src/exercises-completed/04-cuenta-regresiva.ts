bc19cb1889c824ad5638c8383c2fe27e27c91f3b
Junior Saravia <junior.saravia.o@uni.pe>
2021-12-23 23:43:00 -0500

    exercise 4

---
+++ src/exercises-completed/04-cuenta-regresiva.ts
@@ -0,0 +1,35 @@
+import { map, take, tap } from 'rxjs';
+import { interval } from 'rxjs';
+
+/**
+ * Ejercicio: Realizar una cuenta regresiva
+ * empezando de 7
+ */
+
+// Salida esperada ===
+// 7
+// 6
+// 5
+// 4
+// 3
+// 2
+// 1
+// 0
+
+(() =>{
+
+    const inicio = 7;
+    const countdown$ = interval(700).pipe(
+        // Usar los operadores necesarios
+        // para realizar la cuenta regresiva
+        take<number>(inicio+1),    
+        map<number, number>( val => inicio - val )
+    );
+    
+
+    // No tocar esta línea ==================
+    countdown$.subscribe( console.log ); // =
+    // ======================================
+
+
+})();
